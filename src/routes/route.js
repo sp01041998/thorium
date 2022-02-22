866f2ac61d911132d259c3c3c6891659893d2081
Shreyash Patel <spatel01041998@gmil.com>
2022-02-22 15:11:20 +0530

    2nd question done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const obj = require("../logger/loggers.js");
+const helper = require("../util/helpers.js");
 const router = express.Router();
 
 
@@ -11,6 +12,10 @@ router.get('/test-me', function (req, res) {
     console.log(obj.url)
     obj.printWelcomeMessage()
     res.send("hi my name is shreyash")
+
+    helper.printCurrentDate()
+    helper.printCurrentMonth()
+    helper.printBatchInfo()
 })
 
 module.exports = router;
