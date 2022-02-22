b803ccfdc70ad358ab8337d2d85afb37e2bb50a6
Shreyash Patel <spatel01041998@gmil.com>
2022-02-22 11:06:14 +0530

    1st question done

---
+++ src/logger/loggers.js
@@ -0,0 +1,6 @@
+function welcome(name){
+    return "welcome to my application. I am shreyash Patel and a part of function up thorium cohort"
+
+}
+
+module.exports.welcome=welcome;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const obj = require("../logger/loggers.js");
 const router = express.Router();
 
+
+
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+    res.send(obj.welcome("sp"))
 });
 
 module.exports = router;
